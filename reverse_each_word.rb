b6790187f1927beb8e6e3774c4dd1d7d2dd94a0a
Erick Castille <ecastille924@gmail.com>
2020-09-16 22:44:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 #sentance1 = ("Hello there, and how are you?")
 
 def reverse_each_word(sentance1)
-  array = sentance1.split
+  array = sentance1.split("")
   new_array = []
   array.each do |flip|
     new_array << flip.reverse("")
