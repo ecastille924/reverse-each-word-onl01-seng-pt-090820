f24241e5d60b765166045467c98ae4bb0c9fb18e
Erick Castille <ecastille924@gmail.com>
2020-09-16 22:50:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,9 @@
 def reverse_each_word(sentance1)
   array = sentance1.split(" ")
   new_array = []
-  array.each do |flip|
-    new_array << flip.reverse(" ")
+  array.each do|flip|
+    new_array << flip.reverse
   end
   new_array.join(" ")
 end
-
 
