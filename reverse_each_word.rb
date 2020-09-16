391b0d3c533ef8b18bd2c89742e1c062f164ca0d
Erick Castille <ecastille924@gmail.com>
2020-09-16 22:53:09 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,11 @@ def reverse_each_word(sentance1)
   new_array.join(" ")
 end
 
+def reverse_each_word(sentance1)
+  array = sentance1.split(" ")
+  new_array = []
+  array.collect do|flip|
+    new_array << flip.reverse
+  end
+  new_array.join(" ")
+end
